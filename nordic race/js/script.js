598cd3d2c364ae5a947a0da9b057deb23a975332
Mikkel Faartoft Jensen <mikkelfaartoftjensen@gmail.com>
2019-10-28 13:51:31 +0100

    pushup works plus counter

--- nordic race/js/script.js
+++ nordic race/js/script.js
@@ -1,4 +1,14 @@
-// hej //
-$(document).ready(function () {
-  $('.popup').addClass
-    });
+  $(document).ready(function(){
+    //display number on sign
+    i = 0;
+    var player = $("#player");
+    $(".up").click(function(){
+      if (player.attr("src") == "images/armNed.svg") {
+      $(".score").text(i += 1);
+      $("#player").attr('src',"images/armOp.svg");
+    }});
+$(".down").click(function(){
+    $("#player").attr('src',"images/armNed.svg");
+})
+ 
+  });
